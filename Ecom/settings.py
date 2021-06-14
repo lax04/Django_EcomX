3bf92788f513f865eaaca54e314af1032adfccbe
lax04 <laxmiskin04@gmail.com>
2021-06-15 00:53:31 +0530

    deploy1

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6#_1z*4%waf$$xo5+1-rh1bvch-n@)%dk4)msoeuf5jj#tb8(g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', 'ecom-x.herokuapp.com/']
 
 
 # Application definition
