ec0cf216b2ce5122c88ae16a1c3a0666c06a2614
lax04 <laxmiskin04@gmail.com>
2021-06-15 12:14:13 +0530

    sds

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -125,10 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import os
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+WHITENOISE_USE_FINDERS = True
