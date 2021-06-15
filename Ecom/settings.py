f60795294020b6df2cdbf4f6974ebe21a0497481
lax04 <laxmiskin04@gmail.com>
2021-06-15 12:44:49 +0530

    final i guess

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
